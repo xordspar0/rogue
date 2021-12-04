76c27622f85fae7276c5d6481c2a114fbe7954dc
Jordan Christiansen <xordspar0@gmail.com>
2021-12-04 17:57:54 -0600

    Bisect the floor into sections
    Co-authored-by: randomguyeatspotato <randomguyeatspotato@gmail.com>

--- rogue.c
+++ rogue.c
@@ -12,6 +12,11 @@ void print_int(int i)
 	mvaddstr(0, 0, buffer);
 }
 
+int split(int a)
+{
+	return a / 2;
+}
+
 int main(void)
 {
 	initscr();
@@ -31,14 +36,25 @@ int main(void)
 	char floor_background_array[width][height];
 	floor.array = (char *)floor_background_array;
 	clear_floor(floor);
-	new_room(floor, 5, 2, 7, 5);
-	new_room(floor, 25, 22, 7, 5);
-	new_hallway(floor, (int[][2]) { {8, 7}, {0, 10}, {10, 0}, {0, -5},
-		    {-3, 0}
-		    }, 5);
+
+	int a = split(height);
+	new_room(floor,
+		1, 1,
+		width - 2, a - 1);
+	int b = split(width);
+	new_room(floor,
+		1, a + 1,
+		b - 1, height - a - 2);
+	new_room(floor,
+		b + 1, a + 1,
+		width - b - 2, height - a - 2);
+	// new_room(floor, 25, 22, 7, 5);
+	// new_hallway(floor, (int[][2]) { {8, 7}, {0, 10}, {10, 0}, {0, -5},
+	//          {-3, 0}
+	//          }, 5);
 
 	player p = { 10, 5 };
-	monster r = { 27, 24, 'r', *giant_rat_update };
+	monster r = { 34, 3, 'r', *giant_rat_update };
 	for (int c = 0; c != 'q'; c = getch()) {
 		player_input(floor, &p, c);
 		r.update(floor, &r, p);
