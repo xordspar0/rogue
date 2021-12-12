517f43611de94d53db1cb49da85aa74f9e863add
Jordan Christiansen <xordspar0@gmail.com>
2021-12-11 18:09:58 -0600

    Add wasd as alternate arrow keys
    Co-authored-by: randomguyeatspotato <randomguyeatspotato@gmail.com>

--- player.c
+++ player.c
@@ -5,24 +5,28 @@
 void player_input(Floor floor, player * p, int c)
 {
 	switch (c) {
+	case 'w':
 	case KEY_UP:
 		if (floor_walkable(floor, p->x, p->y - 1)) {
 			p->y--;
 		}
 		break;
 
+	case 's':
 	case KEY_DOWN:
 		if (floor_walkable(floor, p->x, p->y + 1)) {
 			p->y++;
 		}
 		break;
 
+	case 'a':
 	case KEY_LEFT:
 		if (floor_walkable(floor, p->x - 1, p->y)) {
 			p->x--;
 		}
 		break;
 
+	case 'd':
 	case KEY_RIGHT:
 		if (floor_walkable(floor, p->x + 1, p->y)) {
 			p->x++;
